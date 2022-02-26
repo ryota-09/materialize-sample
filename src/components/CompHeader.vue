87c6f45ce1eb1273bfb84b0c2fdb4c1225f26a44
ryota-09 <n.ryo950911@gmail.com>
2022-02-26 16:17:47 +0900

    header実装

---
+++ src/components/CompHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <!-- この部分はヘッダー -->
+      <nav>
+        <div class="nav-wrapper blue lighten-1">
+          <a href="#" class="brand-logo">Title</a>
+          <!-- この部分がハンバーガーメニュー -->
+          <a href="" class="sidenav-trigger" data-target="mobile-menu"></a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#">My Page</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class CompHeader extends Vue {}
+</script>
+
+<style scoped></style>
---
+++ src/views/Top.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="top">
+    <CompHeader></CompHeader>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import CompHeader from "@/components/CompHeader.vue";
+@Component({
+  components: {
+    CompHeader
+  }
+})
+export default class XXXComponent extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
